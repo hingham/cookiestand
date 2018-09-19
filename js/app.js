205a0014de23772d54966c05a74c51fbac4bf7eb
hingham <hannah.c.ingham@gmail.com>
2018-09-19 14:39:04 -0700

    added code to calculate total sales for each hour and put that in a new row

--- js/app.js
+++ js/app.js
@@ -6,6 +6,12 @@ var timeArray = ['6am', '7am', '8am', '9am', '10am',
 
 var locations = []; //does this serve for the same thing as Store.locations = []; ??
 
+var hourTotal = [];
+
+for (var z = 0; z<timeArray.length; z++){
+  hourTotal[z] = 0;
+}
+
 function numCustomers(max, min){
   return Math.floor(Math.random()* (max-min + 1)+min);
 }
@@ -40,7 +46,7 @@ Store.prototype.cookiesByHour = function(){
     console.log(mult);
     this.cookiesArray.push(mult);
   }
-  return this.cookiesArray;
+  //return this.cookiesArray;
 };
 
 
@@ -72,21 +78,18 @@ new Store(11, 38, 3.7, 'Seattle-Center');
 new Store(20, 38, 2.3, 'Capitol-Hill');
 new Store(2, 16, 4.6, 'Alki');
 
-
-for (var i = 0; i < locations.length; i++){
-  locations[i].customerByHour() ;
-  locations[i].cookiesByHour() ;
-  console.log(locations[i].totalSales() );
-}
-
+//Create the table head
 var newHead = document.createElement('thead');
 var position = document.getElementsByTagName('table')[0];
 position.appendChild(newHead);
 
+
+//create the table tr in the head
 var newRow = document.createElement('tr');
 position = document.getElementsByTagName('thead')[0];
 position.appendChild(newRow);
 
+//use a function to go through and populate the first row with times
 var tableTimes = function(){
   var firstColumn = document.createElement('th');
   var columnName = document.createTextNode('Store Names');
@@ -108,14 +111,12 @@ var tableTimes = function(){
 
 };
 
-tableTimes();
-
+//create the body for the table
 var tBody = document.createElement('tbody');
 position = document.getElementsByTagName('table')[0];
 position.appendChild(tBody);
 
-var newFoot = document.createElement('tfoot');
-position.appendChild(newFoot);
+
 
 
 Store.prototype.renderDataRow = function () {
@@ -129,13 +130,13 @@ Store.prototype.renderDataRow = function () {
   newStore.appendChild(storeName);
   newRow.appendChild(newStore);
   //shouldn't have to use locations--just this.name--it knows what instance it is
-  var salesData = this.cookiesByHour();
-  salesData.push(this.total);
-  console.log(salesData);
+  //var salesData = this.cookiesByHour();//generate the array cookiesArray
+  //salesData.push(this.total);
+  //console.log(salesData);
   //again, should use this.cookiesByHour
   for (var h = 0; h < timeArray.length; h++){
     var hourSale = document.createElement('td');
-    var saleData = document.createTextNode(salesData[h]);
+    var saleData = document.createTextNode(this.cookiesArray[h]);
     hourSale.appendChild(saleData);
     // position = document.getElementsByTagName('tr')[l]; //now this part is now making sense
     newRow.appendChild(hourSale);//newRow is the parent, append the a child ('td')
@@ -148,8 +149,50 @@ Store.prototype.renderDataRow = function () {
 
 };
 
-locations[0].renderDataRow();
-locations[1].renderDataRow();
+
+//Call funtion to fill in first row with times
+tableTimes();
+
+//locations[0].renderDataRow();
+var renderTable = function() { 
+  for(var t = 0; t < locations.length; t++){
+    locations[t].customerByHour();
+    //this gives us all the data for the number of cookies
+    locations[t].cookiesByHour();//returns an array cookieArray
+    locations[t].renderDataRow();
+
+    //change the value of hourTotal with an updated value based on cookies array for location t
+    for (var c = 0; c < timeArray.length; c++){
+      hourTotal[c] = hourTotal[c] + locations[t].cookiesArray[c];
+    }
+    console.log('hour total' + hourTotal);
+  }
+
+  var newFoot = document.createElement('tfoot');
+  position = document.getElementsByTagName('table')[0];
+  position.appendChild(newFoot);
+
+  var newFootRow = document.createElement('tr');
+  newFoot.appendChild(newFootRow);
+
+
+  var footLabel = document.createElement('td');
+  var footTotalLabel = document.createTextNode('Hourly Totals');
+  footLabel.appendChild(footTotalLabel);
+  newFootRow.appendChild(footLabel);
+
+  // //fill the array hourTotal with the cookies
+  for (var f = 0; f<timeArray.length; f++){
+    var newHourTotal = document.createElement('td');
+    var theHourTotal = document.createTextNode(hourTotal[f]);
+    newHourTotal.appendChild(theHourTotal);
+    newFootRow.appendChild(newHourTotal);
+  }
+
+  //console.log('this should have 15 values: ' + hourTotal);
+};
+
+renderTable();
 
 
 
